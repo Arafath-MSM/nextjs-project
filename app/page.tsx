af5c1bdde3712979aa784d848e9f7509658fd29a
Arafath MSM <139915083+Arafath-MSM@users.noreply.github.com>
2025-11-14 00:13:52 +0530

    Fix formatting in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-
+ 
 export default function Home() {
   return (
     <div className="bg-white dark:bg-black min-h-screen">
